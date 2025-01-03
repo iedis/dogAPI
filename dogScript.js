ea6284843f936e538f5faaea4d00dfb507c5bf9c
Edis Perchiata <edisperchiata@Ediss-MacBook-Pro.local>
2025-01-03 14:19:37 +0200

    add vertical mirroring

--- dogScript.js
+++ dogScript.js
@@ -1,8 +1,10 @@
+let imgUrl;
+let img;
 fetch('https://dog.ceo/api/breeds/image/random')
     .then(response => response.json())
     .then(data => {
-        const imgUrl = data.message;
-        const img = new Image();
+        imgUrl = data.message;
+        img = new Image();
         img.src = imgUrl;
         img.onload = async function (){
             let myPromise = new Promise(function(resolve){
@@ -19,4 +21,16 @@ fetch('https://dog.ceo/api/breeds/image/random')
             document.getElementById("doggy").innerHTML = await myPromise;
         }
     })
+async function VerticalMirror(){
+    let verticalPromise = new Promise(function(resolve){
+        setTimeout(function(){
+        const canvas = document.getElementById('doggy');
+        const ctx = canvas.getContext('2d');
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.drawImage(img, 0, 0,canvas.width, canvas.height);
+        ctx.restore();},
+        1000);
+    });
+    document.getElementById("doggy").innerHTML = await verticalPromise;
+}
 
