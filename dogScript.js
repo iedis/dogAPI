7f12a3a1c767f2eb02ede4ce219aa5fe8c846d71
Edis Perchiata <edisperchiata@Ediss-MacBook-Pro.local>
2025-01-03 13:28:11 +0200

    add async await function for displaying photo

--- dogScript.js
+++ dogScript.js
@@ -4,15 +4,19 @@ fetch('https://dog.ceo/api/breeds/image/random')
         const imgUrl = data.message;
         const img = new Image();
         img.src = imgUrl;
-        img.onload = function(){
-            const canvas = document.getElementById('doggy');
-            const ctx = canvas.getContext('2d');
-            ctx.clearRect(0, 0, canvas.width, canvas.height);
-            ctx.save();
-            ctx.scale(-1,1);
-            ctx.drawImage(img, -canvas.width,0,canvas.width, canvas.height);
-            ctx.restore();
+        img.onload = async function (){
+            let myPromise = new Promise(function(resolve){
+                setTimeout(function(){
+                const canvas = document.getElementById('doggy');
+                const ctx = canvas.getContext('2d');
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
+                ctx.save();
+                ctx.scale(-1,1);
+                ctx.drawImage(img, -canvas.width,0,canvas.width, canvas.height);
+                ctx.restore();},
+                5000);
+            });
+            document.getElementById("doggy").innerHTML = await myPromise;
         }
-
     })
 
